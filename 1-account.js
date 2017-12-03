e619ee8742b37bf0ef7bae74249c3bef383ce8fc
Pasha Rumkin <dev@rumk.in>
2017-12-03 05:13:18 +0300

    [lib,src] update account generator with random option

--- 1-account.js
+++ 1-account.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Web3 = require('web3');
+const crypto = require('crypto');
 
 const {run} = require('./lib/utils.js');
 const prompt = require('./lib/prompt.js');
@@ -12,10 +13,16 @@ async function main() {
     } = await prompt();
 
     // Create 32 byte length hash from password
-    const secretKey = Web3.utils.soliditySha3({
-        type: 'string',
-        value: password,
-    });
+    let secretKey;
+    if (password) {
+        secretKey = Web3.utils.soliditySha3({
+            type: 'string',
+            value: password,
+        });
+    }
+    else {
+        secretKey = '0x' + crypto.randomBytes(32).toString('hex');
+    }
 
     // Convert ethers to weis
     const weis = Web3.utils.toWei(balance.toString(), 'ether');
